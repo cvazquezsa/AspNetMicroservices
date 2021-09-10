245f40256cebb26a8db32d8ad0aa325fbc5df176
Carlos Vazquez <cvazquezsa@outlook.com>
2021-09-10 18:59:24 -0500

    Basket Microservices developed

---
+++ src/Services/Basket/Basket.API/Respositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Respositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+        
+    }
+}
