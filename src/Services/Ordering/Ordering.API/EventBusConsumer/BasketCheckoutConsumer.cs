d9e53ee5c33f183f184a29b60eaba783e168998d
Carlos Vazquez <cvazquezsa@outlook.com>
2021-09-30 12:14:03 -0500

    se integra BacketChekoutConsumer

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
+        {
+            this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this._mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(command);
+
+            logger.LogInformation("BasketCheckoutEvent consumed succesfully. Created Order Id: {newOrderId}", result);
+        }
+    }
+}
